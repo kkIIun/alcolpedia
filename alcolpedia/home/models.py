dd1273b802b85924ddff80b05d716450c332d2a4
kijung <rlwjd4177@naver.com>
2020-07-30 22:33:24 +0900

    models_define

---
+++ alcolpedia/home/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from mdeditor.fields import MDTextField
+from django.utils import timezone
+from member.models import Member
+
+# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.title
+
+
+class Content(models.Model):
+    title = models.CharField(max_length = 200)
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = MDTextField()
+    dated_at = models.DateTimeField(auto_now_add = True)
+    tag = models.ManyToManyField(Tag,related_name='hashtag')
+    like = models.ManyToManyField(User,related_name='likers')
+    summary = models.CharField(max_length = 50)
+    Difficulty = models.IntegerField(null=True,default=0)
+    image = models.ImageField(upload_to="content/", blank=True, null=True)
+    # intro = models.AutoField()
+
+    def __str__(self):
+        return self.title
+
+
+class Bookmark(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
--- alcolpedia/member/models.py
+++ alcolpedia/member/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Member(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 # Create your models here.
