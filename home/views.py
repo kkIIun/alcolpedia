ef3e1fa451ae32dcbb95a777d30ff3fdd862b5ae
unknown <cjy0565@kookmin.ac.kr>
2020-08-22 22:37:10 +0900

    fix_home

--- home/views.py
+++ home/views.py
@@ -11,18 +11,27 @@ def home(request):
     contents_list_len = len(contents_list)
     contents_list = contents_list[:min(3,contents_list_len)]
 
+    bgm_list = Content.objects.filter(sort = "bgm").order_by('updated_at')
+    
+    bgm_list_len = len(bgm_list)
+    bgm_listt = bgm_list[:min(3,bgm_list_len)]
+
     try:
         profile = get_object_or_404(Profile, user__username = request.user.username)
-        return render(request, 'home.html',{'title': 'Alcolpedia','profile':profile,'tags':tag, 'contents': contents_list})
+        return render(request, 'home.html',{'title': 'Alcolpedia','profile':profile,'tags':tag, 'contents': contents_list, 'bgms': bgm_list})
     except :
-        return render(request,'home.html',{'title': 'Alcolpedia','tags':tag, 'contents': contents_list})
+        return render(request,'home.html',{'title': 'Alcolpedia','tags':tag, 'contents': contents_list, 'bgms': bgm_list})
 
 #검색기능
 def search(request) :
     q = request.GET.get('q')
     if q :
         contents = Content.objects.filter(title__icontains=q)
-        return render(request,'search.html',{'contents':contents})
+        try:
+            profile = get_object_or_404(Profile, user__username = request.user.username)
+            return render(request,'search.html',{'contents':contents, 'q':q,'profile':profile})
+        except:
+            return render(request,'search.html',{'contents':contents, 'q':q})
     else : 
         return render(request,'search.html')
 # def detail(request, content_id):
