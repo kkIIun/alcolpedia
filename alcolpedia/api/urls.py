7be2f87263b7f2252a22fa1bd882f88ae84cc210
leekijung <rlwjd4177@naver.com>
2020-11-08 23:44:07 +0900

    feat: api app initial commit

--- alcolpedia/api/urls.py
+++ alcolpedia/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path("articles/", views.contents_function, name = "articles"),
+    path("profile/", views.profile_function, name = "profile"),
+    path("tag/", views.tag_function, name = "tag"),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
