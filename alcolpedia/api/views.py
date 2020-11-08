e6cf522de26d0ab8b2bddd8b11d673da8e7f155f
leekijung <rlwjd4177@naver.com>
2020-11-09 00:31:32 +0900

    fix: error

--- alcolpedia/api/views.py
+++ alcolpedia/api/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render
-from rest_framework import viewsets, pagination
-from .serializers import ProfileSerializer, UserSerializer
+# from django.shortcuts import render
+# from rest_framework import viewsets, pagination
+# from .serializers import ProfileSerializer, UserSerializer
+# from member.models import Profile
+# from article.models import Content
+# from django.contrib.auth.models import User
+# from django.core import serializers
+# from django.http import HttpResponse
+# from rest_framework.decorators import api_view, permission_classes, authentication_classes
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+
+# @api_view(['GET', 'POST'])
+# @permission_classes((IsAuthenticated, ))
+# @authentication_classes((JSONWebTokenAuthentication,))
+# def contents_function(request):
+#     contents = Content.objects.filter(updated_at__isnull=False).order_by('updated_at')
+#     content_list = serializers.serialize('json', contents)
+#     return HttpResponse(content_list, content_type="text/json-comment-filtered")
+
+from rest_framework import viewsets
+from .serializers import UserSerializer,ProfileSerializer,ContentSerializer
 from member.models import Profile
 from article.models import Content
 from django.contrib.auth.models import User
-from django.core import serializers
-from django.http import HttpResponse
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-@api_view(['GET', 'POST'])
-@permission_classes((IsAuthenticated, ))
-@authentication_classes((JSONWebTokenAuthentication,))
-def contents_function(request):
-    contents = Content.objects.filter(updated_at__isnull=False).order_by('updated_at')
-    content_list = serializers.serialize('json', contents)
-    return HttpResponse(content_list, content_type="text/json-comment-filtered")
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
+class ContentViewSet(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import  url, include
-from django.urls import path
-from rest_framework import routers
-from . import views
-
-urlpatterns = [
-    path("articles/", views.contents_function, name = "articles"),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
